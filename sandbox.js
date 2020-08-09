812296bb6f2c92ff4b1c4f65ef85837ac616b6fa
kouzzi010 <kouzzibrhm@gmail.com>
2020-08-10 00:53:43 +0100

    explicit types

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,23 @@
-var circleArea = function (diameter) { return diameter * Math.PI; };
-console.log(circleArea(8));
+//Explicit Types
+var character;
+var age;
+var isLoggedIn;
+//Array
+var arr = [];
+arr.push("hello");
+//union types
+var mixed = [];
+mixed.push("world");
+mixed.push(10);
+console.log(mixed);
+var uid;
+uid = '10';
+uid = 10;
+//Objects
+var obj;
+obj = { name: 'zak', age: 10 };
+var obj2;
+obj2 = {
+    mane: 'tutu',
+    age: 20
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,2 +1,33 @@
-const  circleArea = (diameter: number) => diameter * Math.PI
-console.log(circleArea(8));
+//Explicit Types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+//Array
+let arr: string[] = [];
+arr.push("hello");
+
+//union types
+let mixed: (string|number)[] = [];
+mixed.push("world");
+mixed.push(10);
+console.log(mixed);
+
+let uid: string|number;
+uid = '10';
+uid = 10;
+
+//Objects
+let obj: object;
+obj = {name:'zak', age:10};
+
+let obj2: {
+  name: string,
+  age: number
+};
+
+obj2 = {
+  mane: 'tutu',
+  age: 20
+};
+
